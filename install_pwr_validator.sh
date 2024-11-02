23eac2dffacf71be2826bca4e449ca3c22e32b30
0xgrey <33593974+arcxteam@users.noreply.github.com>
2024-11-02 23:10:37 +0700

    Update install_pwr_validator.sh

--- install_pwr_validator.sh
+++ install_pwr_validator.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Update sistem dan instal Java Development Kit (JDK)
+# Gas bang 😎 Update dan instal (JDK) v17 ini kompatibel
 echo "Updating system and installing Java..."
 sudo apt update && sudo apt install -y openjdk-17-jdk
 
-# Set up environment variables for Java
+# Set up env Java
 export JAVA_HOME=/usr/bin/java
 export PATH=$PATH:$JAVA_HOME
 
-# Mengatur firewall untuk membuka port yang diperlukan
 echo "Configuring firewall rules..."
 sudo ufw allow 8231/tcp
 sudo ufw allow 8085/tcp
@@ -18,12 +17,11 @@ sudo iptables -A INPUT -p tcp --dport 8231 -j ACCEPT
 sudo iptables -A INPUT -p udp --dport 7621 -j ACCEPT
 sudo ufw reload
 
-# Ambil versi terbaru dari validator.jar
+# Auto ya versi terbaru dari validator.jar
 echo "Fetching the latest version of validator.jar..."
 latest_version=$(curl -s https://api.github.com/repos/pwrlabs/PWR-Validator/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
 sudo wget -O /root/validator.jar "https://github.com/pwrlabs/PWR-Validator/releases/download/$latest_version/validator.jar"
 
-# Unduh config.json ke /root
 echo "Creating or downloading config.json..."
 sudo wget -O /root/config.json https://github.com/pwrlabs/PWR-Validator/raw/refs/heads/main/config.json
 
@@ -35,17 +33,17 @@ sudo tee /usr/local/bin/start-pwr-validator.sh > /dev/null <<EOL
 export JAVA_HOME=/usr/bin/java
 export PATH=\$PATH:\$JAVA_HOME
 
-# Jalankan validator dengan konfigurasi
+# Gas jalankan validator dengan konfigurasi
 exec /usr/bin/java -jar /root/validator.jar password <YOUR_SERVER_IP> --compression-level 3 --config /root/config.json
 EOL
 
-# Replace <YOUR_SERVER_IP> dengan alamat IP server yang sebenarnya
+# Replace <YOUR_SERVER_IP> dengan alamat IP server VPS kalian
 sudo sed -i "s|<YOUR_SERVER_IP>|$(curl -s ifconfig.me)|" /usr/local/bin/start-pwr-validator.sh
 
 # Berikan izin eksekusi pada skrip start-up
 sudo chmod +x /usr/local/bin/start-pwr-validator.sh
 
-# Buat file layanan systemd untuk PWR Validator
+# Untuk mwmbuat file layanan systemd PWR Validator
 echo "Creating systemd service file..."
 sudo tee /etc/systemd/system/pwr.service > /dev/null <<EOL
 [Unit]
@@ -62,9 +60,9 @@ Restart=on-failure
 StandardOutput=journal
 StandardError=journal
 
-# Batasan Penggunaan CPU dan RAM
 MemoryLimit=2G
 CPUQuota=100%
+TasksMax=10000
 
 [Install]
 WantedBy=multi-user.target
@@ -76,7 +74,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable pwr.service
 sudo systemctl start pwr.service
 
-# Menampilkan status layanan
+# Tara 😅 Menampilkan status layanan
 echo "PWR Validator service status:"
 sudo systemctl status pwr.service
 sudo journalctl -u pwr.service -f
